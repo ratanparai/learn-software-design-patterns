7a0c7af65f6b7cf87033d2f74243f59827d8ff25
Ratan Sunder Parai <ratanparai@gmail.com>
2017-06-13 21:36:05 +0600

    initial commit
    Templete for dotnet project with tests. I am trying to follow guideline described in https://github.com/dotnet/corefx/ for dotnet core implementations.

---
+++ Strategy pattern/C#/Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace Calculator.Tests
+{
+    class CalculatorTests
+    {
+        // A test for Calculator addition
+        [Fact]
+        public void CalculatorAdditionTest()
+        {
+            // create objects here
+
+            // case 1:  
+            int expected = 4;
+            int actual = 2+2;
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
